d82d3387942545a4981b971614344ac58ceb8320
Jordan Judt <jordanj@daycos.com>
2019-03-29 11:14:00 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoolExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
